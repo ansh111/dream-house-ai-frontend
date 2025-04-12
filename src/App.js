3ac81b69a05fc97b64723495baed19c396abd850
Anshul Nigam <anshulnigam111@gmail.com>
2025-04-12 21:04:11 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// File: src/App.js
+import React from 'react';
+import PropertyFinder from './PropertyFinder';
+
+function App() {
+  return (
+    <div className="App">
+      <PropertyFinder />
+    </div>
+  );
+}
+
+export default App;
